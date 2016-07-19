67d7cd2ac15f1182a832e518a508cdce663dced2
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-19 22:49:37 +1000

    add location list prototype in the view

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
