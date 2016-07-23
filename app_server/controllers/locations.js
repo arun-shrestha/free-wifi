9dc9b94477a4cda5b047b15f715c8e001caff3ca
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-23 19:19:22 +1000

    move info from view to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,82 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+        title: 'Free-Wifi - find a place to work with wifi',
+        pageHeader: {
+            title: 'Free-Wifi',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find placesto work when out and about. Perhaps with coffee, cake or a pint? Let Free-Wifi help you find the place you're looking for.",
+        locations: [{
+            name: 'Starcups',
+            address: '7 Kelly Street, Ultimo, 2007',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+		},{
+            name: 'James Cafe',
+            address: '7 Pitt Street, Sydney, 2000',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+		},{
+            name: 'Three Wise Monkeys',
+            address: '125 George Street, Sydney, 2000',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+		}]
+    });
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-res.render('location-info', { title: 'Location info' });
+    res.render('location-info', { 
+        title: 'Free-Wifi - find a place to work with wifi',
+        pageHeader: {title: 'Free-Wifi'}, 
+		sidebar: {
+			context: 'is on Free-Wifi because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+		},
+		location: {
+			name: 'Starcups',
+			address: '7 Kelly Street, Ultimo, 2007',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			coords: {lat: -33.881742, lng: 151.195569},
+			openingTimes: [{
+				days: 'Monday - Friday',
+				opening: '7:00am',
+				closing: '7:00pm',
+				closed: false
+			},{
+				days: 'Saturday',
+				opening: '8:00am',
+				closing: '5:00pm',
+				closed: false
+			},{
+				days: 'Sunday',
+				closed: true
+			}],
+			reviews: [{
+				author: 'Arun Shrestha',
+				rating: 5,
+				timestamp: '16 July 2016',
+				reviewText: 'What a great place. I can\'t say enough good things about it.'
+			},{
+				author: 'Some Guy',
+				rating: 3,
+				timestamp: '16 June 2016',
+				reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+			}]
+		}
+    });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-res.render('location-review-form', { title: 'Add review' });
+    res.render('location-review-form', {
+		title: 'Review Starcups on Free-Wifi',
+			pageHeader: { title: 'Review Starcups' } 
+		});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res){
-res.render('generic-text', { title: 'About' });
+  res.render('generic-text', { 
+    title: 'About Free-Wifi',
+    content: 'Free-Wifi is created to help people find places to sit down and get a bit of work done. You can contribute to this site by simply giving your review and rating on the businesses that are listed here. If you are a service provider with free wifi to share, you can improve your exprosure by adding details of your business in our page. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
+  });
 };
