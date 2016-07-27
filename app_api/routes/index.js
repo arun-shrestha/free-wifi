1c338c53515f50cec207f9e742d5b2d4bc826e4c
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-27 20:07:56 +1000

    add deleteLocation to api controller

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -8,7 +8,7 @@ router.get('/locations', ctrlLocations.locationsListByDistance);
 router.post('/locations', ctrlLocations.locationsCreate);
 router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
 router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-//router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
 
 // reviews
 router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
