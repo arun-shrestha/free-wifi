3327b6122241b4143e5a93b6cd2fad029f52f326
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-27 18:05:46 +1000

    add module geo.Near in api controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -34,8 +34,33 @@ module.exports.locationsListByDistance = function(req, res) {
         maxDistance: theEarth.getRadsFromDistance(20),
         num: 10
     };
-    Loc.geoNear(point, geoOptions, callback);
+    if (!lng || !lat) {
+        sendJsonResponse(res, 404, {
+            "message": "lng and lat query parameters are required"
+        });
+    return;
+    }
+
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations = [];
+        if (err) {
+            sendJsonResponse(res, 404, err);
+        } else {
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRads(doc.dis),
+                    name: doc.obj.name,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id
+                });
+            });
+            sendJsonResponse(res, 200, locations);
+        }
+    });
 };
+
 
 module.exports.locationsReadOne = function(req, res) {
     if (req.params && req.params.locationid && req.params.reviewid) {
