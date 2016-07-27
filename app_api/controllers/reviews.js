06ee6153660217187c3c0683a6732051a9e2928d
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-27 19:22:50 +1000

    add addReview method in api controller

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,6 +6,57 @@ var sendJsonResponse = function(res, status, content) {
     res.json(content);
 };
 
+
+var doAddReview = function(req, res, location) {
+    if (!location) {
+        sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+        });
+    } else {
+        location.reviews.push({
+            author: req.body.author,
+            rating: req.body.rating,
+            reviewText: req.body.reviewText
+        });
+        location.save(function(err, location) {
+            var thisReview;
+            if (err) {
+                sendJsonResponse(res, 400, err);
+            } else {
+                updateAverageRating(location._id);
+                thisReview = location.reviews[location.reviews.length - 1];
+                sendJsonResponse(res, 201, thisReview);
+            }
+        });
+    }
+};
+
+
+var doAddReview = function(req, res, location) {
+    if (!location) {
+        sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+        });
+    } else {
+        location.reviews.push({
+            author: req.body.author,
+            rating: req.body.rating,
+            reviewText: req.body.reviewText
+        });
+        location.save(function(err, location) {
+            var thisReview;
+            if (err) {
+                sendJsonResponse(res, 400, err);
+            } else {
+                updateAverageRating(location._id);
+                thisReview = location.reviews[location.reviews.length - 1];
+                sendJsonResponse(res, 201, thisReview);
+            }
+        });
+    }
+};
+
+
 module.exports.reviewsCreate = function(req, res) {
     var locationid = req.params.locationid;
     if (locationid) {
