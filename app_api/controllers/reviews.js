4395105eca7fb8fca665eabb5218a8c7c6edb17e
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-27 19:31:55 +1000

    add setAverageRating to api controller

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -7,28 +7,37 @@ var sendJsonResponse = function(res, status, content) {
 };
 
 
-var doAddReview = function(req, res, location) {
-    if (!location) {
-        sendJsonResponse(res, 404, {
-            "message": "locationid not found"
-        });
-    } else {
-        location.reviews.push({
-            author: req.body.author,
-            rating: req.body.rating,
-            reviewText: req.body.reviewText
-        });
-        location.save(function(err, location) {
-            var thisReview;
+var doSetAverageRating = function(location) {
+    var i, reviewCount, ratingAverage, ratingTotal;
+    if (location.reviews && location.reviews.length > 0) {
+        reviewCount = location.reviews.length;
+        ratingTotal = 0;
+        for (i = 0; i < reviewCount; i++) {
+            ratingTotal = ratingTotal + location.reviews[i].rating;
+        }
+        ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+        location.rating = ratingAverage;
+        location.save(function(err) {
             if (err) {
-                sendJsonResponse(res, 400, err);
+                console.log(err);
             } else {
-                updateAverageRating(location._id);
-                thisReview = location.reviews[location.reviews.length - 1];
-                sendJsonResponse(res, 201, thisReview);
+                console.log("Average rating updated to", ratingAverage);
             }
         });
     }
+};
+
+var updateAverageRating = function(locationid) {
+    Loc
+        .findById(locationid)
+        .select('rating reviews')
+        .exec(
+            function(err, location) {
+                if (!err) {
+                    doSetAverageRating(location);
+                }
+            }
+        );
 };
 
 
