e5751ad285b6dc5c5b94b751370ca267cfd3973b
Arun Shrestha <shresthaarun@hotmail.com>
2016-07-29 16:11:23 +1000

    fix location distance

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,63 +1,49 @@
-var mongoose = require( 'mongoose' );
-
+var mongoose = require('mongoose');
+var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Free-Wifi';
-
 if (process.env.NODE_ENV === 'production') {
   dbURI = 'mongodb://shresthaarun:shrestha1@ds042379.mlab.com:42379/free-wifi';
 }
+
 mongoose.connect(dbURI);
 
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose connected to ' + dbURI);
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
 });
 
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose connection error: ' + err);
-});
-
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose disconnected');
-});
-
-
-var gracefulShutdown;
-gracefulShutdown = function (msg, callback) {
-  mongoose.connection.close(function () {
-    console.log('Mongoose disconnected through ' + msg);
-    callback();
-  });
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
-
-var readLine = require ("readline");
-if (process.platform === "win32"){
-  var rl = readLine.createInterface ({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.on ("SIGINT", function (){
-    process.emit ("SIGINT");
-  });
-}
-
 // For nodemon restarts
-process.once('SIGUSR2', function () {
-  gracefulShutdown('nodemon restart', function () {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
-
 // For app termination
 process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function () {
-    process.exit(0);
-  });
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
 });
-
 // For Heroku app termination
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function () {
-    process.exit(0);
-  });
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
 
+// BRING IN YOUR SCHEMAS & MODELS
 require('./locations');
